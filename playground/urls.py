7a8daf5bfa1f678f90f40d812574d4fccf0622c8
Valter Nestor <valterzed.07@gmail.com>
2022-10-28 16:30:54 +0200

    28-10-22

--- playground/urls.py
+++ playground/urls.py
@@ -1,8 +1,12 @@
-from django.urls import path
 from . import views
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.urls import include, path
+from django.views.generic.base import RedirectView
+from django.conf import settings
 
 #URLConf
 urlpatterns = [
     path("", views.render_home),
-    path("loggingIn/", views.logged_in)
+    path("loggingIn/", views.logged_in),
+    path("gen/", views.generate)
 ]
--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext
 import json
 import os
 
@@ -17,37 +18,38 @@ def say_hello(request):
 
 def render_home(request):
     ip = get_client_ip(request)
-    with open(r"playground\data\users.txt", "r+") as json_file:
+    with open(r"playground\data\users.json", "r+") as json_file:
         users = json.loads(json_file.read())
-
-    if ip not in users:
+    
+    if ip not in users.keys():
         users[ip] = {"name": False}
-        a = (r"playground\data\users\ ").replace(" ","")
-        os.mkdir(f"{a}{ip}")
     
     if users[str(ip)]["name"]:
         loggedInWithName = True
     else:
         loggedInWithName = False
 
-    with open(r"playground\data\users.txt", "w+") as json_file:
+    with open(r"playground\data\users.json", "w+") as json_file:
         json_file.write(json.dumps(users))
 
-    return render(request,"home.html", {"LoggedInWithName": loggedInWithName})
+    return render(request,"home.html", {"LoggedInWithName": loggedInWithName},)
 
 def logged_in(request):
     ip = get_client_ip(request)
     
-    with open(r"playground\data\users.txt", "r+") as json_file:
+    with open(r"playground\data\users.json", "r+") as json_file:
         print(json_file)
         users = json.loads(json_file.read())
 
 
-    users[str(ip)]["name"] == "request.GET.get(, False)"
+    users[str(ip)]["name"] == request.GET.get("", False)
     print(users[str(ip)]["name"])
     
-    with open(r"playground\data\users.txt", "w+") as json_file:
+    with open(r"playground\data\users.json", "w+") as json_file:
         json_file.write(json.dumps(users))
     
     return render(request, "redirect.html", {"link": "/f/"})
-    
+
+def generate(request):
+    a = (request.POST.get("query"))
+    return render(request,"home.html",  RequestContext(request))
