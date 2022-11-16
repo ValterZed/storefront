db51ad572df1a9cf8f1a7f6dbd538a7ad18461b4
Valter Nestor01~ <ValterZed.07@gmail.com01~>
2022-11-16 13:18:28 +0100

    Debug = False

--- storefront/settings.py
+++ storefront/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!nc^(dh!s&jfyzj^cp1wurspu83tv=jto+tkz9ib2b(7^568*n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
