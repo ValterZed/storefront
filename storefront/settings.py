d855d16e342d82fe2f7e53394603146363d325fb
Valter Nestor <ValterZed.07@gmail.com>
2022-11-27 12:52:49 +0100

    collected static

--- storefront/settings.py
+++ storefront/settings.py
@@ -120,6 +120,10 @@ USE_TZ = True
 
 BASE_DIR=os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
